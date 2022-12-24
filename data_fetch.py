293dc54f9b601ff95580ce9bafd16669d76a73ef
Kodbollen <anders@bladefoged.com>
2022-12-24 11:19:48 +0100

    Add team to stat-scraper.

--- data_fetch.py
+++ data_fetch.py
@@ -7,7 +7,27 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 
 FPL_url = "https://fantasy.premierleague.com/statistics"
-fbref_urls = ['https://fbref.com/en/squads/18bb7c10/Arsenal-Stats', 'https://fbref.com/en/squads/b8fd03ef/Manchester-City-Stats', 'https://fbref.com/en/squads/b2b47a98/Newcastle-United-Stats', 'https://fbref.com/en/squads/361ca564/Tottenham-Hotspur-Stats', 'https://fbref.com/en/squads/19538871/Manchester-United-Stats', 'https://fbref.com/en/squads/822bd0ba/Liverpool-Stats', 'https://fbref.com/en/squads/d07537b9/Brighton-and-Hove-Albion-Stats', 'https://fbref.com/en/squads/cff3d9bb/Chelsea-Stats', 'https://fbref.com/en/squads/fd962109/Fulham-Stats', 'https://fbref.com/en/squads/cd051869/Brentford-Stats', 'https://fbref.com/en/squads/47c64c55/Crystal-Palace-Stats', 'https://fbref.com/en/squads/8602292d/Aston-Villa-Stats', 'https://fbref.com/en/squads/a2d435b3/Leicester-City-Stats', 'https://fbref.com/en/squads/4ba7cbea/Bournemouth-Stats', 'https://fbref.com/en/squads/5bfb9659/Leeds-United-Stats', 'https://fbref.com/en/squads/7c21e445/West-Ham-United-Stats', 'https://fbref.com/en/squads/d3fd31cc/Everton-Stats', 'https://fbref.com/en/squads/e4a775cb/Nottingham-Forest-Stats', 'https://fbref.com/en/squads/33c895d4/Southampton-Stats', 'https://fbref.com/en/squads/8cec06e1/Wolverhampton-Wanderers-Stats']
+fbref_urls = [('https://fbref.com/en/squads/18bb7c10/Arsenal-Stats', 'ARS'),
+              ('https://fbref.com/en/squads/b8fd03ef/Manchester-City-Stats', 'MCI'),
+              ('https://fbref.com/en/squads/b2b47a98/Newcastle-United-Stats', 'NEW'),
+              ('https://fbref.com/en/squads/361ca564/Tottenham-Hotspur-Stats', 'TOT'),
+              ('https://fbref.com/en/squads/19538871/Manchester-United-Stats', 'MUN'),
+              ('https://fbref.com/en/squads/822bd0ba/Liverpool-Stats', 'LIV'),
+              ('https://fbref.com/en/squads/d07537b9/Brighton-and-Hove-Albion-Stats', 'BHA'),
+              ('https://fbref.com/en/squads/cff3d9bb/Chelsea-Stats', 'CHE'),
+              ('https://fbref.com/en/squads/fd962109/Fulham-Stats', 'FUL'),
+              ('https://fbref.com/en/squads/cd051869/Brentford-Stats', 'BRE'),
+              ('https://fbref.com/en/squads/47c64c55/Crystal-Palace-Stats', 'CRY'),
+              ('https://fbref.com/en/squads/8602292d/Aston-Villa-Stats', 'AVL'),
+              ('https://fbref.com/en/squads/a2d435b3/Leicester-City-Stats', 'LEI'),
+              ('https://fbref.com/en/squads/4ba7cbea/Bournemouth-Stats', 'BOU'),
+              ('https://fbref.com/en/squads/5bfb9659/Leeds-United-Stats', 'LEE'),
+              ('https://fbref.com/en/squads/7c21e445/West-Ham-United-Stats', 'WHU'),
+              ('https://fbref.com/en/squads/d3fd31cc/Everton-Stats', 'EVE'),
+              ('https://fbref.com/en/squads/e4a775cb/Nottingham-Forest-Stats', 'NFO'),
+              ('https://fbref.com/en/squads/33c895d4/Southampton-Stats', 'SOU'),
+              ('https://fbref.com/en/squads/8cec06e1/Wolverhampton-Wanderers-Stats', 'WOL')]
+
 
 def setup_driver(site_url):
     # Setting up chromedriver
@@ -48,16 +68,16 @@ def scrape_player_values():
             pts = table_data[5].text
             print(f'{name:<14}, {team}, {pos}, {value}, {pts}')
 
-    #         player_list.append([name, country, pos, values[i]])
+            player_list.append([name, team, pos, value, pts])
 
-    # driver.close()
-    # return np.array(player_list)
+    driver.close()
+    return np.array(player_list)
 
 
 def scrape_player_stats():
     player_list = []
     # for url in ["https://fbref.com/en/squads/a2d435b3/Leicester-City-Stats"]:
-    for i, url in enumerate(fbref_urls):
+    for i, (url, team) in enumerate(fbref_urls):
         print(f'Scraping {i+1} of {len(fbref_urls)}\n{url.split("/")[-1]}')
         try:
             driver = setup_driver(url)
@@ -83,6 +103,7 @@ def scrape_player_stats():
                 stats.append(row.find_element(By.XPATH, './td[@data-stat="npxg_xg_assist"]').text)
 
                 player.append(name)
+                player.append(team)
                 player.append(country)
 
 
@@ -103,11 +124,11 @@ def scrape_player_stats():
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'value':
         value_info = scrape_player_values()
-        df = pd.DataFrame(value_info, columns=['name', 'country', 'position', 'value'])
+        df = pd.DataFrame(value_info, columns=['name', 'team', 'position', 'value', 'points'])
         df.to_csv('value_info.csv', index=False)
 
     if len(sys.argv) > 1 and sys.argv[1] == 'stats':
         stat_info = scrape_player_stats()
-        df = pd.DataFrame(stat_info, columns=['name', 'country', 'goals', 'assists', 'xg', 'xa', 'npxg_xa'])
+        df = pd.DataFrame(stat_info, columns=['name', 'team', 'country', 'goals', 'assists', 'xg', 'xa', 'npxg_xa'])
         df.to_csv('stat_info.csv', index=False)
 
